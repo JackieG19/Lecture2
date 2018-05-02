23ba37c8cae4a39e8f9f5ad8de199fd8529f2422
Jackie <JGtheophila19@gmail.com>
2018-05-02 10:33:38 -0400

    Create initials.c

---
+++ initials.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<cs50.h>
+
+int main(void)
+{
+    string name = get_string("Name: ");
+    char initials[4];
+
+    for (int i = 0; i = strlen(name); i++)
+    {
+        if (isupper(name[i]))
+        {
+            initials[1] = name[i];
+        }
+    }
+
+}
